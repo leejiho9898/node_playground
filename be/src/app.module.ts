dee468af3d2edd8b7be4646b846c24d477488389
leejiho9898 <cctv1998@naver.com>
2026-02-22 16:10:29 +0900

    feat: 디비 연동

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserEntity } from "./entities/user.entity";
 import { LoginModule } from "./modules/login/login.module";
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "mysql",
+        host: config.get<string>("DB_HOST", "localhost"),
+        port: parseInt(config.get<string>("DB_PORT") ?? "3306", 10),
+        username: config.get<string>("DB_USERNAME", "root"),
+        password: config.get<string>("DB_PASSWORD", "root"),
+        database: config.get<string>("DB_DATABASE", "test"),
+        entities: [UserEntity],
+        synchronize: true,
+      }),
+    }),
+    LoginModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
